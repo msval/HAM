47ea30ccd363ba0fb703220942907ff4fc431267
Marko Svaljek <msvaljek@gmail.com>
2014-05-11 22:54:26 +0200

    added add new capability

--- routes/sensors.js
+++ routes/sensors.js
@@ -2,11 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var pg = require('pg');
-var pgConString = "postgres://msvaljek:5432@localhost/msvaljek";
+var pgConString = "postgres://";
 
-router.get('/', function(req, res) {
+router.get('/', showAllSensors);
+
+router.post('/add', function (req, res) {
+	pg.connect(pgConString, function (err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err);
+		}
+		console.log('help');
+		console.log(req.param('name'));
+
+		var query = client.query('INSERT INTO sensor(name) VALUES($1)', [req.param('name')]);
+		
+		return res.redirect('/sensors');
+	});
+});
+
+function showAllSensors(req, res) {
 	pg.connect(pgConString, function(err, client, done) {
-		if(err) {
+		if (err) {
 			return console.error('error fetching client from pool', err);
 		}
 
@@ -24,6 +40,6 @@ router.get('/', function(req, res) {
 
 		});
     });
-});
+}
 
 module.exports = router;
